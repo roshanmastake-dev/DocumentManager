9521ea39b38367d29f5fddf7529a2df143cf56c1
roshanmastake-dev <roshanmastake1564@gmail.com>
2026-06-22 09:45:12 +0530

    Initialize settings.gradle.kts for project configuration

---
+++ settings.gradle.kts
@@ -0,0 +1,33 @@
+pluginManagement {
+
+    repositories {
+
+        google()
+
+        mavenCentral()
+
+        gradlePluginPortal()
+
+    }
+
+}
+
+dependencyResolutionManagement {
+
+    repositoriesMode.set(
+        RepositoriesMode.FAIL_ON_PROJECT_REPOS
+    )
+
+    repositories {
+
+        google()
+
+        mavenCentral()
+
+    }
+
+}
+
+rootProject.name = "DocumentManager"
+
+include(":app")
