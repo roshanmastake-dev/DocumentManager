f38c05b002015aa1fb22878c50e027de21c71aac
roshanmastake-dev <roshanmastake1564@gmail.com>
2026-06-22 10:51:30 +0530

    Add Routes object for navigation constants

---
+++ app/src/main/java/com/roshan/documentmanager/navigation/Routes.kt
@@ -0,0 +1,15 @@
+package com.roshan.documentmanager.navigation
+
+object Routes {
+
+    const val HOME = "home"
+
+    const val CATEGORY = "category"
+
+    const val IMPORT = "import"
+
+    const val DOCUMENT = "document"
+
+    const val SETTINGS = "settings"
+
+}
