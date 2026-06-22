3d0f59dd9b3f09be0b0def90768b8a8bc8241c7f
roshanmastake-dev <roshanmastake1564@gmail.com>
2026-06-22 11:06:20 +0530

    Implement AddCategoryUseCase for adding categories

---
+++ app/src/main/java/com/roshan/documentmanager/domain/usecase/AddCategoryUseCase.kt
@@ -0,0 +1,34 @@
+package com.roshan.documentmanager.domain.usecase
+
+import com.roshan.documentmanager.data.local.entity.CategoryEntity
+import com.roshan.documentmanager.data.repository.CategoryRepository
+
+class AddCategoryUseCase(
+
+    private val repository: CategoryRepository
+
+) {
+
+    suspend operator fun invoke(
+
+        name: String,
+
+        parentId: Long?
+
+    ) {
+
+        repository.insert(
+
+            CategoryEntity(
+
+                name = name,
+
+                parentId = parentId
+
+            )
+
+        )
+
+    }
+
+}
