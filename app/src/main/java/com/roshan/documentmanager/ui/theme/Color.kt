0c31165765224f07e1b1fb1a3daa4bdf7b24cb66
roshanmastake-dev <roshanmastake1564@gmail.com>
2026-06-22 09:51:28 +0530

    Add color definitions for theme

---
+++ app/src/main/java/com/roshan/documentmanager/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.roshan.documentmanager.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF1A73E8)
+val Secondary = Color(0xFF4285F4)
+val Background = Color(0xFFF8F9FA)
+val Surface = Color(0xFFFFFFFF)
+
+val TextPrimary = Color(0xFF202124)
+val TextSecondary = Color(0xFF5F6368)
+
+val CardColor = Color(0xFFFFFFFF)
+
+val Success = Color(0xFF34A853)
+val Error = Color(0xFFEA4335)
