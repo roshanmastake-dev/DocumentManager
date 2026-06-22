a23d21105dee5332ccf110e6cc7051cc5981795c
roshanmastake-dev <roshanmastake1564@gmail.com>
2026-06-22 09:47:21 +0530

    Add plugins for Android and Kotlin in build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+// Top-level build file
+
+plugins {
+    id("com.android.application") version "8.6.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.google.dagger.hilt.android") version "2.56.2" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+}
