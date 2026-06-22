59a151fd4b57a3467d8e903171fb16f218cd889b
roshanmastake-dev <roshanmastake1564@gmail.com>
2026-06-22 10:37:24 +0530

    Add Constants object for database and navigation keys

---
+++ app/src/main/java/com/roshan/documentmanager/utils/Constants.kt
@@ -0,0 +1,13 @@
+package com.roshan.documentmanager.utils
+
+object Constants {
+
+    const val DATABASE_NAME = "document_manager.db"
+
+    const val HOME = "home"
+
+    const val CATEGORY = "category"
+
+    const val DOCUMENT = "document"
+
+}
