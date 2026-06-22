9009f4a700897686b5f6b412cc50a82361afe07c
roshanmastake-dev <roshanmastake1564@gmail.com>
2026-06-22 09:50:20 +0530

    Create MainActivity for Document Manager app

---
+++ app/src/main/java/com/roshan/documentmanager/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.roshan.documentmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+            Surface(
+
+                color = MaterialTheme.colorScheme.background
+
+            ) {
+
+            }
+
+        }
+
+    }
+
+}
