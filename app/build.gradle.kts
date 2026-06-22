36b3305b0666a6a36aae6fad888bd7da271a1d35
roshanmastake-dev <roshanmastake1564@gmail.com>
2026-06-22 09:48:02 +0530

    Add build.gradle.kts for Android project setup

---
+++ app/build.gradle.kts
@@ -0,0 +1,107 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+
+    namespace = "com.roshan.documentmanager"
+
+    compileSdk = 36
+
+    defaultConfig {
+
+        applicationId = "com.roshan.documentmanager"
+
+        minSdk = 24
+
+        targetSdk = 36
+
+        versionCode = 1
+
+        versionName = "1.0"
+
+        testInstrumentationRunner =
+            "androidx.test.runner.AndroidJUnitRunner"
+
+        vectorDrawables {
+
+            useSupportLibrary = true
+
+        }
+
+    }
+
+    buildTypes {
+
+        release {
+
+            isMinifyEnabled = false
+
+            proguardFiles(
+
+                getDefaultProguardFile(
+                    "proguard-android-optimize.txt"
+                ),
+
+                "proguard-rules.pro"
+
+            )
+
+        }
+
+    }
+
+    compileOptions {
+
+        sourceCompatibility = JavaVersion.VERSION_17
+
+        targetCompatibility = JavaVersion.VERSION_17
+
+    }
+
+    kotlinOptions {
+
+        jvmTarget = "17"
+
+    }
+
+    buildFeatures {
+
+        compose = true
+
+    }
+
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.16.0")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
+
+    implementation("androidx.activity:activity-compose:1.10.1")
+
+    implementation("androidx.compose.ui:ui:1.8.3")
+
+    implementation("androidx.compose.material3:material3:1.3.2")
+
+    implementation("androidx.navigation:navigation-compose:2.9.0")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.1")
+
+    implementation("io.coil-kt:coil-compose:2.7.0")
+
+    implementation("androidx.room:room-runtime:2.7.2")
+
+    implementation("androidx.room:room-ktx:2.7.2")
+
+    ksp("androidx.room:room-compiler:2.7.2")
+
+    implementation("com.google.dagger:hilt-android:2.56.2")
+
+    ksp("com.google.dagger:hilt-compiler:2.56.2")
+
+}
