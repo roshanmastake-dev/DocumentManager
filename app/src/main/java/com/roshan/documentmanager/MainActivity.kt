bd671715647db34c41887c3fe762f09ce5d63895
roshanmastake-dev <roshanmastake1564@gmail.com>
2026-06-23 16:33:59 +0530

    Refactor MainActivity to use AppNavGraph

--- app/src/main/java/com/roshan/documentmanager/MainActivity.kt
+++ app/src/main/java/com/roshan/documentmanager/MainActivity.kt
@@ -3,22 +3,104 @@ package com.roshan.documentmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.roshan.documentmanager.navigation.AppNavigation
-import com.roshan.documentmanager.ui.theme.DocumentManagerTheme
+import androidx.navigation.compose.rememberNavController
+import com.roshan.documentmanager.navigation.AppNavGraph
+import com.roshan.documentmanager.ui.screen.backup.BackupScreen
+import com.roshan.documentmanager.ui.screen.category.CategoryDetailScreen
+import com.roshan.documentmanager.ui.screen.category.CategoryDetailUiState
+import com.roshan.documentmanager.ui.screen.document.DocumentDetailScreen
+import com.roshan.documentmanager.ui.screen.document.DocumentDetailUiState
+import com.roshan.documentmanager.ui.screen.home.HomeScreen
+import com.roshan.documentmanager.ui.screen.settings.SettingsScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(
+        savedInstanceState: Bundle?
+    ) {
 
         super.onCreate(savedInstanceState)
 
         setContent {
 
-            DocumentManagerTheme {
+            val navController =
+                rememberNavController()
 
-                AppNavigation()
+            AppNavGraph(
 
-            }
+                navController = navController,
+
+                homeScreen = {
+
+                    HomeScreen()
+
+                },
+
+                categoryScreen = {
+
+                    CategoryDetailScreen(
+
+                        state = CategoryDetailUiState(),
+
+                        onCategoryClick = {},
+
+                        onDocumentClick = {},
+
+                        onAddClick = {}
+
+                    )
+
+                },
+
+                documentScreen = {
+
+                    DocumentDetailScreen(
+
+                        state = DocumentDetailUiState(),
+
+                        onRename = {},
+
+                        onShare = {},
+
+                        onPrint = {},
+
+                        onDelete = {}
+
+                    )
+
+                },
+
+                settingsScreen = {
+
+                    SettingsScreen(
+
+                        onBackupClick = {},
+
+                        onPrivacyClick = {},
+
+                        onAboutClick = {}
+
+                    )
+
+                },
+
+                backupScreen = {
+
+                    BackupScreen(
+
+                        loading = false,
+
+                        onBackupClick = {},
+
+                        onRestoreClick = {}
+
+                    )
+
+                }
+
+            )
 
         }
 
