a729e18286f260137db567fdd9a3b7e06f924ac4
roshanmastake-dev <roshanmastake1564@gmail.com>
2026-06-23 17:29:52 +0530

    Remove Android and Kotlin plugin declarations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
-plugins {
-    id("com.android.application") version "8.6.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
-    id("com.google.dagger.hilt.android") version "2.56.2" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
-}
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+    <style name="Theme.DocumentManager" parent="android:Theme.Material.Light.NoActionBar">
+    </style>
+</resources>
